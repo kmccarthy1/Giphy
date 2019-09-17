fba6969aa61ef1519a29172a2ec1d0734d0fa0e9
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-09-17 14:29:05 -0400

    fixed function

--- assets/site.js
+++ assets/site.js
@@ -15,7 +15,7 @@ function renderButtons() {
         b.addClass("food-btn");
         b.attr("data-name", foodArray[i]);
         b.text(foodArray[i]);
-        
+
         $("#buttons-view").append(b);
     }
 };    
@@ -25,11 +25,9 @@ function displayRatingInfo() {
 
     var h = $(this).attr('data-name');
     let giphykey = "GI1pI1MGLn9asxU97K9yx3v7WJDl107W";
-    // console.log(this);
     
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + h + "&api_key=" + giphykey + "&limit=10";
-    // console.log(queryURL);
-    // Creating an AJAX call for the specific hero button being clicked
+
     $.ajax({
         url:queryURL, 
         method: 'GET'
@@ -40,10 +38,10 @@ function displayRatingInfo() {
         $('#food-view').empty();
 
             for(var i=0; i < results.length; i++) {
-            // Creating a div to hold the heroes-form
+            // Creating a div to hold the food form grp
                 var foodDiv = $("<div class='foodH'>"); 
                 var rating = results[i].rating;
-                // console.log(rating);
+
                 var p = $("<p>").text("Rating: " + rating);
                 var foodImage = $("<img>");
 
@@ -57,34 +55,19 @@ function displayRatingInfo() {
                 foodDiv.prepend(foodImage);
 
                 $('#food-view').prepend(foodDiv);
-
-                // $('#food-view').prepend("<p>Rating: " + response.data[i].rating + "</p>");
-                // $('#food-view').prepend("<img src='" + response.data[i].images.fixed_width_still.url + "'>");
             }
         });
 };
 
-// $('#buttons-view').on('click', '.gif', displayRatingInfo) 
-
 // This function handles events when the submit button is clicked
 $('#add-food').on('click', function(event) {
-    // alert("Button Clicked!");
 
     // event.preventDefault() prevents the form from trying to submit itself.
     // We're using a form so that the user can hit enter instead of clicking the button if they want
     event.preventDefault();
-
-    // This line will grab the text from the input box
     var food = $("#food-input").val().trim();
-    // console.log(hero);
 
-    // The superhero entered from the textbox is then added to our array
     foodArray.push(food);
-
-    // Putting the entire form above the previous gifs
-    // $("#heroes-form").prepend(heroesDiv);
-
-    // calling renderButtons which handles the processing of our superHeroes array
     renderButtons();
 });
 
@@ -105,12 +88,7 @@ $('#food-view').on('click', '.pause', function() {
     }
  });
 
-// ================EVENTS=================================
-// $(document).on('click', '#add-hero');
 
-// Added a click event listener to all elements with a class of "hero-btn"
 $(document).on('click', '.food-btn', displayRatingInfo);
 
-
-// calling the renderButtons function
 renderButtons();
